4a966715aa65abbbb3dc3f1e9c78efb24607f731
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-22 19:26:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,24 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  
+ scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+  
+scale.index(2)
+  
 end
 
 def find_max_value(array)
   # Add your solution here
+   hill = [1,2,3,4,5,4,3,2,1]
+   
+   hill.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  scale = [2,3,4,5]
+  scale.min
 end
